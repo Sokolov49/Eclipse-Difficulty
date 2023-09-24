59bf83d504ce9e8dfc0fa0d44171ca2fe91e482b
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2023-09-24 22:21:31 +0200

    Update hudmanagerpd2.lua

--- lua/hudmanagerpd2.lua
+++ lua/hudmanagerpd2.lua
@@ -3,4 +3,9 @@ if string.lower(RequiredScript) == "lib/managers/hudmanagerpd2" then
 		self._hud_temp:show_carry_bag( carry_id, value )
 		self._sound_source:post_event( "Play_bag_generic_pickup" )
 	end
+
+	function HUDManager:temp_hide_carry_bag()
+		self._hud_temp:hide_carry_bag()
+		self._sound_source:post_event( "Play_bag_generic_throw" )
+	end
 end
