220a02f7db0dc448d653992cae787d3ee804d402
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2023-12-23 14:35:50 +0100

    follow latest

--- req/mission_script/pbr.lua
+++ req/mission_script/pbr.lua
@@ -1,10 +1,44 @@
 return {
-	[100119] = {
-		set_ponr_state = true
-	},
-	[101483] = {
+	[100104] = {
 		values = {
 			enabled = false
+		  }
+	},
+	[100980] = {
+		ponr = {
+			length = 60,
+			player_mul = {1.67, 1.34, 1, 1}
 		}
-	}
+	},
+	-- slow down a bunch of ground level spawnpoints
+	[100411] = {
+		values = {
+			interval = 20
+		}
+	},
+	[100403] = {
+		values = {
+			interval = 20
+		}
+	},
+	[100412] = {
+		values = {
+			interval = 20
+		}
+	},
+	[100413] = {
+		values = {
+			interval = 20
+		}
+	},
+	[100409] = {
+		values = {
+			interval = 20
+		}
+	},
+	[100408] = {
+		values = {
+			interval = 20
+		}
+	},
 }
