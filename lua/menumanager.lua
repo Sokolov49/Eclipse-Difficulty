d79ae72a45371814cda91eb4bc93596262e4d4d1
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-07-18 13:21:13 +0200

    read me
    this has all the ui changes that original pack does for perks but it also changes the skill tree because im a lazy ctrl-c ctrl-v user
    i dont have paypay 2 installed rn to remove the skill tree parts
    it will be removed later, this may kill your game due to missing functions of some skills from original pack, other than many other things
    treat this as a mockup, concept or a template for the perks system, idk

--- lua/menumanager.lua
+++ lua/menumanager.lua
@@ -1114,3 +1114,12 @@ function MenuCallbackHandler:play_safehouse(params)
 		yes_func = yes_func,
 	})
 end
+
+-- original pakc 2
+local data = MenuCallbackHandler._increase_infamous
+function MenuCallbackHandler:_increase_infamous(yes_clbk)
+	data(self, yes_clbk)
+	managers.skilltree:reset_specializations()
+	managers.skilltree._global.specializations.total_points = Application:digest_value(0, true)
+	managers.skilltree._global.specializations.points = Application:digest_value(0, true)
+end
